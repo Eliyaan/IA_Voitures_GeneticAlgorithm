02c0aedb6fe3e86964afd65c716f1b11bfd38eea
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-05-03 22:59:03 +0200

    seems to work

--- codes/voiture.gd
+++ codes/voiture.gd
@@ -1,7 +1,7 @@
 extends Node2D
 var nn = {
 	#Consts
-	"nb_inputs" = 8,
+	"nb_inputs" = 9,
 	"nb_hidden_layer" = 1,
 	"nb_hidden_neurones" = [4],
 	"nb_outputs" = 2,
@@ -20,7 +20,7 @@ func set_rd_wb_values():
 		for j in range(nn["weights_list"][i][0].size()):
 			for k in range(nn["weights_list"][i][0][j].size()):
 				nn["weights_list"][i][0][j][k] = randf_range(-1, 1)
-
+	print(nn["weights_list"])
 	#Biases 
 	for i in range(nn["layers_list"].size()):
 		for j in range(nn["layers_list"][i][0].size()):
@@ -28,7 +28,7 @@ func set_rd_wb_values():
 		
 
 func sigmoid(value):
-	return 1 / (1 +  2.71828^(-value))
+	return 1 / (1 +  2.71828**(-value))
 
 
 func fprop(inputs):
@@ -36,16 +36,15 @@ func fprop(inputs):
 		for j in range(nn["layers_list"][i][2].size()):
 			var nactiv = 0
 			if i == 0:
+				print(nn["weights_list"][i][0][j])
 				for k in range(inputs.size()):  # Pour chaque input
-					print(k)
-					print(inputs[k])
 					nactiv += nn["weights_list"][i][0][j][k] * inputs[k] #Le bon weight fois le bon input
 			else:
 				for k in range(nn["layers_list"][i-1][3].size()):  # Pour chaque input
 					nactiv += nn["weights_list"][i][0][j][k] * nn["layers_list"][i-1][3][k] #Le bon weight fois le bon input	
 			nactiv += nn["layers_list"][i][0][j]  # Ajout du bias
 			nn["layers_list"][i][3][j] = sigmoid(nactiv)  #activation function
-	return nn["layers_list"][nn["nb_hidden_layer"]+1]
+	return nn["layers_list"][nn["nb_hidden_layer"]]
 	
 
 
@@ -64,17 +63,14 @@ func reset():
 func init():
 	nn["weights_list"] = []
 	nn["weights_list"].resize(nn["nb_hidden_layer"]+1)
-	print(nn["weights_list"])
 	for i in range(nn["weights_list"].size()):
 		nn["weights_list"][i] = [[[]],[[]]]
-		print(nn["weights_list"])
 	nn["layers_list"] = []
 	nn["layers_list"].resize(nn["nb_hidden_layer"]+1)
 	for i in range(nn["layers_list"].size()):
 		nn["layers_list"][i] = [[], [], [], [], []]
 	for i in range(0, nn["nb_hidden_layer"]+1):
 		if i == 0:
-			print(nn["weights_list"])
 			nn["weights_list"][i][0] = []
 			nn["weights_list"][i][0].resize(nn["nb_hidden_neurones"][0])
 			for j in range(nn["weights_list"][i][0].size()):
