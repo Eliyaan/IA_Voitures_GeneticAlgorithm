2fa5328b321d75d0c827691bc10a406f1963bb48
Linklancien <linklancien@gmail.com>
2023-05-04 11:44:17 +0200

    Spawn des Voitures

--- codes/main.gd
+++ codes/main.gd
@@ -1,11 +1,19 @@
 extends Node2D
-
+@export var voitures : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	#Gen voitures
+	for _voiture in range(0, 1):
+		spawn_voitures()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+	
+func spawn_voitures():
+	var car
+	car = voitures.instantiate()
+	car.position.x = 0
+	car.position.y = 0
+	$Voitures.add_child(car)
