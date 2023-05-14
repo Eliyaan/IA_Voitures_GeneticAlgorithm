2343558c1ef44c30cba700849ad95572df2a2485
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-05-14 13:00:27 +0200

    normalement ca marche
    donc backup

--- codes/main.gd
+++ codes/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 @export var voitures : PackedScene
 var steps: int = 0
-var nb_voitures: int = 100
+var nb_voitures: int = 300
 var nb_offsprings: int = 20
 var running: bool = false
 var sim_steps: int = 300
